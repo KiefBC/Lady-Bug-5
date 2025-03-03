69c899ddf579ae7c67dca1ff7343ab0e26da1ca9
KiefBC <kiefhay642@outlook.com>
2025-03-03 14:28:01 -0800

    Seperation of files.

---
+++ Lady Bug 5/RowView.swift
@@ -0,0 +1,18 @@
+//
+//  RowView.swift
+//  Lady Bug 5
+//
+//  Created by Kiefer Hay on 2025-03-03.
+//
+
+import SwiftUI
+
+struct RowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RowView()
+}
